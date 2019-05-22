4aaa10e872d8e7e5a0f62e41ed707dfce8da0ebd
mikuta0407 <mikuta0407@hs-sh.net>
2019-05-22 10:13:58 +0900

    First

---
+++ Assets/camera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(Camera))]
+public class camera : MonoBehaviour
+{
+	public string nextLevel;
+
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Playerの部分はカメラが追いかけたいオブジェクトの名前をいれる
+        this.player = GameObject.Find("DemoUnityChan2D");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(-1, playerPos.y + 0.5f, transform.position.z);
+		transform.rotation = Quaternion.Euler(0,0,0);
+
+		if (playerPos.y + 0.5f > 89){
+			StartCoroutine(INTERNAL_Clear());
+            enabled = false;
+		}
+    }
+	private IEnumerator INTERNAL_Clear()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player)
+        {
+            player.SendMessage("Clear", SendMessageOptions.DontRequireReceiver);
+        }
+
+        yield return new WaitForSeconds(3);
+		SceneManager.LoadScene ("Start");
+        // Application.LoadLevel(nextLevel);
+    }
+}
