f8c51900190cec8b369eef3883a79eaea7f2bb88
mikuta0407 <flute.takkun0407@gmail.com>
2019-05-24 15:45:25 +0900

    resultに点数表示スクリプトを作成開始

---
+++ Assets/result.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class result : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.log(score);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
