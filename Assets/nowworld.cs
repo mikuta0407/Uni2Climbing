79a21c22fc4f15e7b8d084525363855aa04fdd48
mikuta0407 <flute.takkun0407@gmail.com>
2019-06-19 10:56:40 +0900

    動作中ワールドを保存したり読み込んだりするやつ

---
+++ Assets/nowworld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nowworld : MonoBehaviour {
+	public static string world;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static string getworld(){
+		return world;
+	}
+	public static void setworld(string a){
+		world = a;
+	}
+}
