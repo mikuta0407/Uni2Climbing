be94a9a9776250ae55e177c08df385c8139f133f
mikuta0407 <flute.takkun0407@gmail.com>
2019-06-19 10:57:43 +0900

    祝! score共有実装!

---
+++ Assets/scoresaver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoresaver : MonoBehaviour {
+	public static string score = "0000000";
+	public static string highscore;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static string getscore(){
+		Debug.Log("getscore "+ score);
+		return score;
+	}
+	public static void setscore(string a){
+		score = a;
+		Debug.Log("setscore " + score);
+	}
+
+	public static string gethighscore(){
+		return highscore;
+	}
+	public static void sethighscore(string b){
+		highscore = b;
+	}
+}
