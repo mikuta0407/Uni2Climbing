1d9c071d86d42e252d49aaf6ce929325cbc90e9d
mikuta0407 <flute.takkun0407@gmail.com>
2019-06-19 14:39:54 +0900

    死亡判定回数カウント実装

---
+++ Assets/count.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class count : MonoBehaviour {
+
+	//敵接触回数
+	public static int hit;
+	
+	public static int gethit(){
+		return hit;
+	}
+
+	public static void addhit(int a){
+		hit = a;
+	}
+}
