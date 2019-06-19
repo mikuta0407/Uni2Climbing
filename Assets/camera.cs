8a022e68e9ed51c6e1132b5766c8fa4ed5f20c38
mikuta0407 <flute.takkun0407@gmail.com>
2019-06-19 10:55:51 +0900

    暫定レベル遷移用if分岐追加

--- Assets/camera.cs
+++ Assets/camera.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(Camera))]
 public class camera : MonoBehaviour
 {
-	public string nextLevel;
-
+	public string nowLevel;
+    public string nextLevel;
     GameObject player;
 
     // Use this for initialization
     void Start()
     {
-        // Playerの部分はカメラが追いかけたいオブジェクトの名前をいれる
+        nowLevel=nowworld.getworld();
         this.player = GameObject.Find("DemoUnityChan2D");
     }
 
@@ -41,6 +41,23 @@ public class camera : MonoBehaviour
         if (player)
         {
             player.SendMessage("Clear", SendMessageOptions.DontRequireReceiver);
+        }
+
+        if (nowLevel == "1-1"){
+            nextLevel = "Loading 1-2";
+            nowworld.setworld(nextLevel);
+        } else if (nowLevel == "1-2"){
+            nextLevel = "Loading 1-3";
+            nowworld.setworld(nextLevel);
+        } else if (nowLevel == "1-3"){
+            nextLevel = "Loading 1-4";
+            nowworld.setworld(nextLevel);
+        } else if (nowLevel == "1-4"){
+            nextLevel = "Loading 1-5";
+            nowworld.setworld(nextLevel);
+        } else if (nowLevel == "1-5"){
+            nextLevel = "result";
+            nowworld.setworld(nextLevel);
         }
 
         yield return new WaitForSeconds(3);
