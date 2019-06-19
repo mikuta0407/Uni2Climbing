5c12f5b2376ac6fe8bd9e07087c826910632ede3
mikuta0407 <flute.takkun0407@gmail.com>
2019-06-19 14:52:52 +0900

    クリアフラグの実装

---
+++ Assets/clearflag.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clearflag : MonoBehaviour {
+
+	public static int flag = 0;
+
+	public static int getflag(){
+		return flag;
+	}
+
+	public static void setflag(){
+		flag = 1;
+		// 1ならクリアした 0なら死んだ
+	}
+}
