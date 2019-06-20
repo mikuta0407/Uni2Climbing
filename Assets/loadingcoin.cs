c117b2887cd187e05df48c47c66dc802b3ba2692
mikuta0407 <flute.takkun0407@gmail.com>
2019-06-20 16:22:50 +0900

    coinとscoreを分離､1~3に適用

--- Assets/loadingcoin.cs
+++ Assets/loadingcoin.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class loadingpoint : MonoBehaviour {
-	public GUIText total;
-	
+public class loadingcoin : MonoBehaviour {
+	public GUIText coin;
 	// Use this for initialization
 	void Start () {
-		total.text = scoresaver.getscore();
+		coin.text = scoresaver.getcoin();
 	}
 	
 	// Update is called once per frame
