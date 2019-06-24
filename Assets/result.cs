181364b4497aac7e8ea9e11d4d3ba0e04ef8bb10
mikuta0407 <flute.takkun0407@gmail.com>
2019-06-24 10:33:57 +0900

    RESULT文章実装

--- Assets/result.cs
+++ Assets/result.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class result : MonoBehaviour {
-
+	public GUIText cong;
+	public GUIText cleared;
+	public GUIText yourscore;
+	
 	// Use this for initialization
 	void Start () {
-		// Debug.Log(score);
+		// congratulation
+		// you cleared the game 
+		if (clearflag.getflag() == 1){
+			cong.text = "congratulation!!";
+			cleared.text = "YOU cleared the game!!";
+		} else {
+			cong.text = "GAME OVER";
+		cleared.text = "better luck next time";
+		}
+
+		yourscore.text = "YOUR SCORE IS " +scoresaver.getscore();
 	}
 	
 	// Update is called once per frame
