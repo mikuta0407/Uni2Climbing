e15bdba0b309215b6e0bb6d8e693458612a0ddcd
mikuta0407 <mikuta0407@hs-sh.net>
2019-06-26 11:32:09 +0900

    ゴール判定無事完成!!!

--- Assets/camera.cs
+++ Assets/camera.cs
@@ -13,41 +13,57 @@ public class camera : MonoBehaviour
     GameObject player;
 
     public int goal = 0;
+    bool clear = false;
 
     
     // Use this for initialization
     void Start()
     {
+        //ゴールしてないよ!っていうやつ
         goal = 0;
+        //今動いてるワールドを検出
         nowLevel = nowworld.getworld();
+        //ユニティちゃんを検出(カメラの追従用)
         this.player = GameObject.Find("DemoUnityChan2D");
+        //BGMの再生開始。(BGMはInspectorで設定)
         bgm.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //カメラの位置移動
+        //プレイヤーの位置を取得
         Vector3 playerPos = this.player.transform.position;
+        //カメラの位置を設定(プレイヤーの座標+6)
         transform.position = new Vector3(-1, playerPos.y + 6f, transform.position.z);
+        //普通にやるとユニティちゃんが左右反転でカメラも左右反転で死ぬのでローテーションを固定
 		transform.rotation = Quaternion.Euler(0,0,0);
 
-		if ((playerPos.x <= 0) && (playerPos.y >= 91)){
+        //もしゴール範囲内に入ったら
+		if ((goal == 0) && (playerPos.x <= 0) && (playerPos.y >= 91)){
 			StartCoroutine(INTERNAL_Clear());
-            enabled = false;
+            //enabled = false;
 		}
 
         // FOR DEBUG: FORCE CLEAR
-        if (Input.GetKeyDown(KeyCode.B)) {
+        if ((goal == 0) && (Input.GetKeyDown(KeyCode.B))) {
             StartCoroutine(INTERNAL_Clear());
         }
 
-        if ((goal == 1) && (!se.GetComponent<playse>().goalplaying())){
-            changeScene();
-        }
-
+        //死んでたらBGM止める(クリア時とは別の挙動)
         if (count.gethit() == 3){
             bgm.Stop();
         }
+
+        
+        // ゴールジングル鳴り終わったらシーン遷移
+        if ((clear) && (goal == 1) && (!se.GetComponent<playse>().goalplaying())){
+            Debug.Log("シーンを変えますよ!");
+            changeScene();
+        }
+
+        
 
     }
 	private IEnumerator INTERNAL_Clear()
@@ -60,18 +76,19 @@ public class camera : MonoBehaviour
         scoresaver.setscore( ( (Convert.ToInt32(scoresaver.getscore())) + (timesaver.gettime()*10) ) .ToString("0000000") );
         se.GetComponent<playse>().playgoal();
                 
-        var player = GameObject.FindGameObjectWithTag("Player");
+        //var player = GameObject.FindGameObjectWithTag("Player");
 
-        if (player)
-        {
-            player.SendMessage("Clear", SendMessageOptions.DontRequireReceiver);
-        }
+        //if (player)
+        //{
+        //    player.SendMessage("Clear", SendMessageOptions.DontRequireReceiver);
+        //}
 
         yield return new WaitForSeconds(0);
     }
 
     private void changeScene(){
         
+        clear = true;
         if (nowLevel == "1-1"){
             nextLevel = "1-2";
         } else if (nowLevel == "1-2"){
